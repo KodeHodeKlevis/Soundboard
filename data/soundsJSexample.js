0a8bcc1d2b2f308377aa2d528edf104d78c0de96
KodeHodeKlevis <klevis25sfj@gmail.com>
2025-02-11 11:40:24 +0100

    Update soundsJSexample.js

--- data/soundsJSexample.js
+++ data/soundsJSexample.js
@@ -1,22 +1,11 @@
 export const sounds = [
-  {
-    name: "name of the sound",
-    file: "pathTofile",
-    key: "keyboardKeyTobePressed",
-  },
-  {
-    name: "name of the sound",
-    file: "pathTofile",
-    key: "keyboardKeyTobePressed",
-  },
-  {
-    name: "name of the sound",
-    file: "pathTofile",
-    key: "keyboardKeyTobePressed",
-  },
-  {
-    name: "name of the sound",
-    file: "pathTofile",
-    key: "keyboardKeyTobePressed",
-  },
+  {file:"./data/y2mate_5gbyby1.mp3", key: "Y"},
+  {file:"./data/wrong-answer-sound-effect.mp3", key: "W"},
+  {file:"./data/vine-boom.mp3", key:"V"},
+  {file:"./data/sigma-boy.mp3", key:"S"},
+  {file:"./data/rizz-sound-effect.mp3", key:"R"},
+  {file:"./data/metal-pipe-clang.mp3", key:"M"},
+  {file:"./data/limit-on-talking.mp3", key:"L"},
+  {file:"./data/gta-v-notification.mp3", key:"G"},
+  {file:"./data/drip-goku-meme-song-original-dragon-ball-super-music-clash-of-gods-in-description.mp3", key:"D"},
 ];
